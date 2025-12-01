9da09a5a8cdef4f0f8d72885a35a3b65c1864bd6
ZebaShabnoor <166287401+ZebaShabnoor@users.noreply.github.com>
2025-12-01 11:43:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
- return "Hello, world!"
- 
-if __name__ == "__main__":
- app.run(host='0.0.0.0', port=80)
+from selenium import webdriver
+from time import sleep
+
+    # Now you can use sleep directly
+
+from selenium.webdriver.common.by import By
+
+# Initialize WebDriver (e.g., Chrome)
+driver = webdriver.Chrome()
+
+# Navigate to a webpage
+driver.get("https://www.google.com")
+sleep(3)
+
+# Find the search box by its 'name' attribute using the new method
+search_box = driver.find_element(By.NAME, "q")
+
+# Interact with the element
+search_box.send_keys("Selenium")
+sleep(3)
+driver.get("https://www.selenium.dev")
+
+
+
+search_box.submit()
+
+# Close the browser
+driver.quit()
